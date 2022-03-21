168cb8310917130cf269f5e48a21160b551c64f1
Gabriel Azevedo <gsa384@gmail.com>
2022-03-21 12:24:10 -0300

    Teste Angular

--- SistemaPastelando-UI/src/app/app-routing.module.ts
+++ SistemaPastelando-UI/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { CardapioComponent } from './components/cardapio/cardapio.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'atendimento/cardapio',
+    component: CardapioComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SistemaPastelando-UI/src/app/app.module.ts
+++ SistemaPastelando-UI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardapioComponent } from './components/cardapio/cardapio.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardapioComponent,
+    PedidosComponent
   ],
   imports: [
     BrowserModule,
